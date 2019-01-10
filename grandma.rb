09661fd34be483d66edb2a2528c8d02b4ad6efca
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-10 02:05:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,8 @@ def speak_to_grandma(input)
 
   if input == input.upcase
     return "NO, NOT SINCE 1938!"
-  end
   elsif input == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
