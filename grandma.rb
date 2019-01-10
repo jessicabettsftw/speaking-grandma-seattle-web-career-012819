602391c401357d26702efbf605221c0cec4c960f
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-10 01:57:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(input)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+if input == input.upcase
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-
+  puts "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
-
+end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
